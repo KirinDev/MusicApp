d2597d737014e33abccfe60f82d88fbe75fd7edb
Pedro Ferreira <111713739+KirinDev@users.noreply.github.com>
2023-01-16 19:21:23 +0000

    .

--- src/main/java/app/domain/shared/Constants.java
+++ src/main/java/app/domain/shared/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final String ROLE_USER = "USER";
     public static final String ROLE_ADMIN = "ADMIN";
 
-    public static final String MUSIC_FILE = "data/musicData";
+    public static final String MUSIC_FILE = "data/musicData.txt";
 }
